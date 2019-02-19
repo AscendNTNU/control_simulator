f7aed1fd2cd53ccec1877db47fb91f3b65abd52b
Rendell Cale <rendellc@stud.ntnu.no>
2019-02-19 14:30:30 +0100

    Add more debug print to entrypoint

--- components/builder_entrypoint.sh
+++ components/builder_entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 source /opt/ros/kinetic/setup.bash 
+echo "--- DEBUG PRINTS ---"
+echo $HOME
+echo $USER
+ls $HOME/.ssh
+ls $HOME
 ls ~/.ssh
+echo "--- DEBUG PRINTS ---"
 
 git submodule update --init --recursive
 make build-images
