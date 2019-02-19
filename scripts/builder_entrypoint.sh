a4a120ad92cf9606c35c708e3fd31b9caf43101a
Rendell Cale <rendellc@stud.ntnu.no>
2019-02-19 14:43:47 +0100

    Tweak debug prints

--- scripts/builder_entrypoint.sh
+++ scripts/builder_entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 source /opt/ros/kinetic/setup.bash 
-echo "--- DEBUG PRINTS ---"
+echo "--- DEBUG PRINTS begin ---"
 echo $HOME
-echo $USER
+ls /root/.ssh
 ls $HOME/.ssh
 ls $HOME
 ls ~/.ssh
-echo "--- DEBUG PRINTS ---"
+echo "--- DEBUG PRINTS end ---"
 
 git submodule update --init --recursive
 make build-images
