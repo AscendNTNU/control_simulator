453409444540ef109326c838df009bc53fd553b7
Rendell Cale <rendellc@stud.ntnu.no>
2019-02-19 14:47:23 +0100

    Try using sudo

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker build --rm -f "Dockerfile" -t control_simulator_builder .
-docker run --privileged \
+sudo docker run --privileged \
   -v $HOME/.ssh:/root/.ssh \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):/bin/docker \
