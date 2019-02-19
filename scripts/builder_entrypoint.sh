52b671caae047b4facbc3b79b1d2b29ff6b3eb9f
Rendell Cale <rendellc@stud.ntnu.no>
2019-02-19 18:41:11 +0100

    Remove debug prints

--- scripts/builder_entrypoint.sh
+++ scripts/builder_entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
 source /opt/ros/kinetic/setup.bash 
-echo "--- DEBUG PRINTS begin ---"
-echo $HOME
-ls /root/.ssh
-ls $HOME/.ssh
-ls $HOME
-ls ~/.ssh
-echo "--- DEBUG PRINTS end ---"
-
 git submodule update --init --recursive
 make build-images
 exec ${@}
