d4661bc1a78d95802a66ba77205c6afe58018a61
Rendell Cale <rendellc@stud.ntnu.no>
2019-02-19 17:06:14 +0100

    Remove user specification and use read only volume

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 docker build --rm -f "Dockerfile" -t control_simulator_builder .
-docker run --privileged -u root \
-  -v $HOME/.ssh:/root/.ssh \
+docker run --privileged \
+  -v $HOME/.ssh:/root/.ssh:ro \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):/bin/docker \
   control_simulator_builder
