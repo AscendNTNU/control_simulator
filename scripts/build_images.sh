c38772052e5d66eb6de08b019899fff90a74cc17
Rendell Cale <rendellc@stud.ntnu.no>
2019-02-19 14:21:26 +0100

    Fix ssh paths

--- scripts/build_images.sh
+++ scripts/build_images.sh
@@ -2,7 +2,7 @@
 
 docker build --rm -f "Dockerfile" -t control_simulator_builder .
 docker run --privileged \
-  -v ~/.ssh:/home/root/.ssh \
+  -v $HOME/.ssh:/root/.ssh \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v $(which docker):/bin/docker \
   control_simulator_builder
