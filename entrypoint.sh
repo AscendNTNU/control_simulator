4048955a5cccdf862c7279ae9f9b2c3e9d58653f
Rendell Cale <rendellc@stud.ntnu.no>
2019-03-22 10:01:13 +0100

    Rename mocap node

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 set +e 
 export ROS_IP=$(hostname -i)
-rm -rf /tmp/.X1-lock
-Xvfb :1 -screen 0 1280x720x16 &
-export DISPLAY=:1.0
+#rm -rf /tmp/.X1-lock
+#Xvfb :1 -screen 0 1280x720x16 &
+#export DISPLAY=:1.0
 source $(dpkg -L gazebo7 | grep gazebo/setup.sh)
 source /opt/ros/kinetic/setup.bash
 source devel/setup.bash
