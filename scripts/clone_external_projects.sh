d1d54b33e01e05dca97e089f8e08035b19725f91
Rendell Cale <rendellc@stud.ntnu.no>
2019-04-01 18:03:14 +0200

    Better gazebo (#33)
    * Update drone meshes, remove rotors_description
    
    * Change default world
    
    * Add rosinstall for mavros
    
    * docker build, enforce mavlink/mavros versions

--- scripts/clone_external_projects.sh
+++ scripts/clone_external_projects.sh
@@ -2,6 +2,12 @@
 
 TOKEN=$1
 
+wstool init src
+wstool merge -t src scripts/mavros.rosinstall
+wstool update -t src -j4
+rosdep install --from-paths src --ignore-src -y
+bash src/mavros/mavros/scripts/install_geographiclib_datasets.sh
+
 git clone https://github.com/AscendNTNU/ascend_msgs.git src/ascend_msgs 
 if [ -z "$TOKEN" ]
 then
@@ -10,3 +16,4 @@ else
   git clone https://${TOKEN}:@github.com/AscendNTNU/ascend_utils.git src/ascend_utils
   git clone https://${TOKEN}:@github.com/AscendNTNU/fluid_fsm.git src/fluid_fsm 
 fi
+
